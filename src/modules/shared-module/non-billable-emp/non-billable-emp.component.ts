d08840278bd7795ee79fec62d7a8025f7cad28c8
madhulikapriya1 <madhulika.priya@walkingtree.tech>
2018-07-10 18:16:00 +0530

    local commit

---
+++ src/modules/shared-module/non-billable-emp/non-billable-emp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-non-billable-emp',
+  templateUrl: './non-billable-emp.component.html',
+  styleUrls: ['./non-billable-emp.component.css']
+})
+export class NonBillableEmpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
