7790987cb2b06c3d8228c19f105d05ea0d4e1945
pallavimunnuru <pallavi.munnuru@walkingtree.tech>
2018-07-11 17:55:24 +0530

    add work details and personal details

--- src/modules/add-employee-module/add-employee/personal-details/personal-details.component.ts
+++ src/modules/add-employee-module/add-employee/personal-details/personal-details.component.ts
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-details.component.css']
 })
 export class PersonalDetailsComponent implements OnInit {
+selectedDepartment: string;
+  department;
+  constructor() {
+    this.department = [];
+    this.department.push({label:'Employees', value:{id:1, name: 'Employees', code: 'EMP'}});
+    this.department.push({label:'Utilization', value:{id:1, name: 'Utilization', code: 'UTI'}});
+  }
 
-  constructor() { }
 
   ngOnInit() {
   }
